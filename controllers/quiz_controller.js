f9acfa5d0cb69e4390b7edbe62854aa911ce7d5e
Jose Manuel <jmanuel_1407@hotmail.com>
2015-08-27 13:45:37 -0500

    Creacion de preguntas II

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -2,7 +2,7 @@ var models = require('../models/models.js');
 
 //Autoload - factoriza el código si ruta incluye :quizId
 exports.load = function(req,res,next,quizId){
-	models.Quiz.find(quizId).then(
+	models.Quiz.findById(quizId).then(
 		function(quiz) {
 			if(quiz){
 				req.quiz = quiz;
@@ -16,14 +16,14 @@ exports.load = function(req,res,next,quizId){
 exports.index = function(req, res){
 	models.Quiz.findAll().then(
 		function(quizes){
-			res.render('quizes/index',{quizes: quizes});
+			res.render('quizes/index',{quizes: quizes,errors:[]});
 		}
 	).catch(function(error){next(error);})
 };
 
 //GET /quizes/:id
 exports.show = function(req,res){
-		res.render('quizes/show',{ quiz: req.quiz});
+		res.render('quizes/show',{ quiz: req.quiz,errors:[]});
 };
 
 //GET /quizes/:id/answer
@@ -32,7 +32,7 @@ exports.answer = function(req, res){
 	if (req.query.respuesta === req.quiz.respuesta){
 		resultado='Correcto';
 	}
-	res.render('quizes/answer',{ quiz: req.quiz, respuesta:resultado});
+	res.render('quizes/answer',{ quiz: req.quiz, respuesta:resultado,errors:[]});
 };
 
 //GET /quizes/new
@@ -40,15 +40,23 @@ exports.new=function(req,res){
 	var quiz = models.Quiz.build(//Crea un objeto quiz
 		{pregunta:"Pregunta", respuesta: "Respuesta"}
 		);
-	res.render('quizes/new',{quiz:quiz});
+	res.render('quizes/new',{quiz:quiz,errors:[]});
 };
 
 //POST /quizes/create
 exports.create = function(req,res){
 	var quiz= models.Quiz.build(req.body.quiz);
+	quiz.validate()
+	.then(
+		function(err){
+			if(err){
+				res.render('quizes/new',{quiz:quiz,errors:err.errors});
+			} else{
+			quiz//save: guarda en DB camos pregunta y respuesta de quiz
+			.save({fields:["pregunta","respuesta"]})
+			.then(function(){res.redirect('/quizes')})//Redirección HTTP(URL relativo) lista de preguntas
+		}
+		}
 
-	//guarda en DB los campos pregunta y respuesta de qiz
-	quiz.save({fields:["pregunta","respuesta"]}).then(function(){
-		res.redirect('/quizes');
-	}) //Redirección HTTP(URL relativo) lista de preguntas
+	); 
 };
