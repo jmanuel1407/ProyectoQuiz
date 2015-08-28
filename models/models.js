831cd68890589093447efa06c2be5bab30dc5f28
Jose Manuel <jmanuel_1407@hotmail.com>
2015-08-28 17:25:23 -0500

    Creacion de comentarios

--- models/models.js
+++ models/models.js
@@ -26,10 +26,19 @@ var sequelize = new Sequelize(DB_name, user, pwd,
   }      
 );
 
+//Importar definicion de la tabla quiz
 var quiz_path = path.join(__dirname,'quiz');
 var Quiz = sequelize.import( quiz_path);
 
+//Importar definicion de la tabla comment
+var comment_path = path.join(__dirname,'comment');
+var Comment = sequelize.import(comment_path);
+
+Comment.belongsTo(Quiz);
+Quiz.hasMany(Comment);
+
 exports.Quiz = Quiz;
+exports.Comment = Comment;
 
 // sequelize.sync() inicializa tabla de preguntas en DB
 sequelize.sync().then(function(){
